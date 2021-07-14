f0fc5f2dd151030304d1a2095ea6b2d040c5c76d
lauretteroy <laurieroy.dev@gmail.com>
2021-07-14 10:35:38 -0700

    Add React router, Airline, Airlines components

---
+++ app/javascript/components/Airlines/Airlines.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Airlines = () => {
+	return <div>This is the Airlines#show view</div>
+}
+
+export default Airlines
--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -4,23 +4,15 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import PropTypes from 'prop-types'
-
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
-
-Hello.defaultProps = {
-  name: 'David'
-}
-
-Hello.propTypes = {
-  name: PropTypes.string
-}
+import App from '../components/App'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello name="React" />,
+    <Router>
+      <Route path="/" component={App}/>
+    </Router>,
     document.body.appendChild(document.createElement('div')),
   )
 })
